9b8c999c692f636aad3fb4491a1df3ea6d268404
wdAres <puneetsri9990@gmail.com>
2023-10-11 10:46:08 +0530

    Update Schedule.jsx

--- src/Pages/Schedule/Schedule.jsx
+++ src/Pages/Schedule/Schedule.jsx
@@ -5,6 +5,7 @@ import Heading from '../../Components/Heading/Heading'
 import Calendar from '../../Components/Calender/Calendar'
 import ListCalendar from '../../Components/ListCalendar/ListCalendar'
 import RescheduleModal from '../../Components/AllModals/RescheduleModal/RescheduleModal'
+import { useNavigate } from 'react-router-dom'
 
 const Schedule = () => {
 
@@ -21,10 +22,17 @@ const Schedule = () => {
         }
     }
 
+    const navigate = useNavigate()
+    const changePage = () => {
+        navigate('/sch')
+    }
+
     return (
+
+
         <React.Fragment>
             <PagePath />
-            <Heading heading={'Schedule'} p={'You can see your schedule here and know your classes'} btnFunction={setPopup} btnValue={popup} btn={'Add Session'} />
+            <Heading heading={'Schedule'} p={'You can see your schedule here and know your classes'} btnFunction2={changePage}  btn={'Add Session'} />
             <div className={classes.btn_container}>
                 <button onClick={() => setNum(0)} className={`${num === 0 ? classes.active : ''}`}>Calendar View</button>
                 <button onClick={() => setNum(1)} className={`${num === 1 ? classes.active : ''}`}>List View</button>
