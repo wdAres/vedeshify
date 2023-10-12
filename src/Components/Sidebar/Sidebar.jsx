41e01c23326c77f0eb8dcad1445ec42954aac9e7
wdAres <puneetsri9990@gmail.com>
2023-10-12 13:55:17 +0530

    update

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -5,7 +5,20 @@ import { RxCross2 } from 'react-icons/rx'
 // import dp
 import img from '../../assets/dp.png'
 import logo from '../../assets/logo.png'
+import i1 from "../../assets/Icons/dash_icon.svg";
+import i2 from "../../assets/Icons/schedule_icon.svg";
+import i3 from "../../assets/Icons/plan_biiling_icon.svg";
+import i4 from "../../assets/Icons/services_icon.svg";
+import i5 from "../../assets/Icons/bookings_icon.svg";
+import i6 from "../../assets/Icons/inquiries_icon.svg";
+import i7 from "../../assets/Icons/support_icon.svg";
+import i8 from "../../assets/Icons/payments_icon.svg";
+import i9  from "../../assets/Icons/resource_icon.svg";
+import i10  from "../../assets/Icons/roles_icon.svg";
+import i11  from "../../assets/Icons/profile_icon.svg";
+import i12 from "../../assets/Icons/message_icon.svg";
 
+import {ReactSVG} from 'react-svg';
 const Sidebar = (props) => {
   return (
     <div className={classes.sidebar}>
@@ -17,40 +30,40 @@ const Sidebar = (props) => {
       <div className={classes.middle} onClick={() => { props.onSideberBtn() }}>
         <h5>Generals</h5>
         <NavLink className={classes.link} to={'/dashboard'}>
-          Dashboard
+      <ReactSVG className={classes.svg} src={i1} /> Dashboard
         </NavLink>
         <NavLink className={classes.link} to={'/students'}>
-          Students
+        <ReactSVG className={classes.svg} src={i10} /> Students
         </NavLink>
         <NavLink className={classes.link} to={'/profile'}>
-          Profile
+        <ReactSVG className={classes.svg} src={i11} />  Profile
         </NavLink>
         <NavLink className={classes.link} to={'/payment'}>
-          Payment
+        <ReactSVG className={classes.svg} src={i8} /> Payment
         </NavLink>
         <NavLink className={classes.link} to={'/community'}>
-          Community
+        <ReactSVG className={classes.svg} src={i6} /> Community
         </NavLink>
         <NavLink className={classes.link} to={'/chats'}>
-          Chats
+        <ReactSVG className={classes.svg} src={i12} /> Chats
         </NavLink>
         {/* <NavLink className={classes.link} to={'/schedule'}>
           Schedule
         </NavLink> */}
         <NavLink className={classes.link} to={'/inquiries'}>
-        Inquiries
-        </NavLink>
-        <NavLink className={classes.link} to={'/reports'}>
-          Reports
-        </NavLink>
-        <NavLink className={classes.link} to={'/support'}>
-          Support
+        <ReactSVG className={classes.svg} src={i9} /> Inquiries
         </NavLink>
         <NavLink className={classes.link} to={'/booking'}>
-          Booking
+        <ReactSVG className={classes.svg} src={i2} /> Booking
         </NavLink>
         <NavLink className={classes.link} to={'/services'}>
-          Services
+        <ReactSVG className={classes.svg} src={i4} /> Services
+        </NavLink>
+        <NavLink className={classes.link} to={'/reports'}>
+        <ReactSVG className={classes.svg} src={i7} /> Reports
+        </NavLink>
+        <NavLink className={classes.link} to={'/support'}>
+        <ReactSVG className={classes.svg} src={i11} />  Support
         </NavLink>
         {/* <br />
         <h5>Generals</h5>
